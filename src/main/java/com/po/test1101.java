db1e58dd7f2ecb1d34504ce8899de7b922c81b85
82110 <123@qq.com>
2023-04-20 02:10:02 +0800

    版本四

---
+++ src/main/java/com/po/test1101.java
@@ -0,0 +1,13 @@
+package com.po;
+
+public class test1101 {
+    private Integer eag;
+
+    public test1101(Integer eag) {
+        this.eag = eag;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
